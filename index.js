dd38437967c46f805cc02ac551f07631d2d77054
Durgaprasad2404 <132249475+Durgaprasad2404@users.noreply.github.com>
2024-05-04 18:57:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
 
 const app = express();
 app.use(cookieParser());
-
+app.use(cors())
 dotenv.config({ path: "./config.env" });
 
 require("./db/connection");
