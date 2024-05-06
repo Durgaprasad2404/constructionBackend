5e28f58416e3d9c115d8ac1411706cc7c739f2e9
Durgaprasad2404 <132249475+Durgaprasad2404@users.noreply.github.com>
2024-05-06 15:49:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const cookieParser = require("cookie-parser");
 const cors = require('cors')
 
 const app = express();
-app.use(cookieParser());
 app.use(cors())
+app.use(cookieParser());
+
 dotenv.config({ path: "./config.env" });
 
 require("./db/connection");
