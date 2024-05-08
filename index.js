be5fbf27d2ac65fe17e6ec9577869edc0dc23c5a
dp <nathiprasad2404@gmail.com>
2024-05-08 10:37:56 +0530

    server

--- index.js
+++ index.js
@@ -12,16 +12,7 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://frontendconstruction.netlify.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 
